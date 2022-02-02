45a5f51117eea9ae9e794ef301b6b2e176c1463c
mike <gavrilin.mike@gmail.com>
2022-02-02 08:42:44 +0300

    первый сквош

--- src/main/java/ru/javaops/bootjava/RestaurantVotingApplication.java
+++ src/main/java/ru/javaops/bootjava/RestaurantVotingApplication.java
@@ -28,6 +28,7 @@ UserRepository userRepository;
     //3 commit
     //commit in feature task 1
     //commit idea
+    //for сквош
 
     public static void main(String[] args)  {
         SpringApplication.run(RestaurantVotingApplication.class, args);
